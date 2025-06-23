a983898880f0577ae61813e5bcf96851d9104f42
ApoorvAri <arictirkey227@gmail.com>
2025-06-23 22:41:22 +0530

    Delete Assignments/Assignment-2/Aric/p5.py

--- Assignments/Assignment-2/Aric/p5.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt 
-import pandas as pd 
-df=pd.read_csv(r"C:\Users\SUNNY DAVID\Downloads\student_age_marks.csv")
-plt.scatter(df['Marks'],df['Age'])
-plt.title('Age vs Marks')
-plt.xlabel('Marks')
-plt.ylabel('Age')
-plt.show()
