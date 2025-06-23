2173fcd84e7d0f035d07fc256034e0f77cf393d0
ApoorvAri <arictirkey227@gmail.com>
2025-06-23 22:42:02 +0530

    Delete Assignments/Assignment-2/Aric/p8.py

--- Assignments/Assignment-2/Aric/p8.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-df=pd.DataFrame({'Name':['Aric','Eric','Ariana','Selena','Lana'],'Course':['ME','CSE','CHE','AE','EE'],'Marks':[92,None,46,None,87]})
-filled=df['Marks'].fillna(df['Marks'].mean())
-print(filled)
